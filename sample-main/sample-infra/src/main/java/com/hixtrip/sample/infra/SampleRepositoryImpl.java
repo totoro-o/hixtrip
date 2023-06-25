0a7f07f267b65c9351594b5e5764a716fdb6b318
heph <1213862525@qq.com>
2023-06-25 09:48:07 +0800

    redis配置

--- sample-main/sample-infra/src/main/java/com/hixtrip/sample/infra/SampleRepositoryImpl.java
+++ sample-main/sample-infra/src/main/java/com/hixtrip/sample/infra/SampleRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.hixtrip.sample.infra.db.convertor.SampleDOConvertor;
 import com.hixtrip.sample.infra.db.dataobject.SampleDO;
 import com.hixtrip.sample.infra.db.mapper.SampleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,8 +18,12 @@ public class SampleRepositoryImpl implements SampleRepository {
     @Autowired
     private SampleMapper sampleMapper;
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @Override
     public Sample test() {
+        //redisTemplate.opsForValue().set("test","123");
         //此处可调用mybatis进行查询，DO转化为领域对象返回，如：
         SampleDO sampleDO = sampleMapper.selectSample();
         SampleDO sampleDO1 = sampleMapper.selectById(1L);
---
+++ sample-starter/src/main/java/com/hixtrip/sample/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.hixtrip.sample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    /**
+     * 在该方法中自定义RedisTemplate的序列化器，方法名自己取，记得加@Bean注解返回RedisTemplate对象
+     * @param redisConnectionFactory 连接工厂，Spring会自动注入
+     * @return RedisTemplate对象
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        // 创建 RedisTemplate 对象
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        // 设置连接工厂
+        template.setConnectionFactory(redisConnectionFactory);
+        // 创建JSON序列化工具
+        GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        // 设置KEY的序列化
+        template.setKeySerializer(RedisSerializer.string());
+        template.setHashKeySerializer(RedisSerializer.string());
+        // 设置VALUE的序列化
+        template.setValueSerializer(genericJackson2JsonRedisSerializer);
+        template.setHashValueSerializer(genericJackson2JsonRedisSerializer);
+        // 返回
+        return template;
+    }
+}
