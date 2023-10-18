aefc34d8e275dc9794676afa6cc4b12acc673974
JrJo9 <1197684529@qq.com>
2023-10-18 16:15:03 +0800

    fix:不可锁方法，根据商品ID加锁

--- sample-main/sample-infra/src/main/java/com/hixtrip/sample/infra/InventoryRepositoryImpl.java
+++ sample-main/sample-infra/src/main/java/com/hixtrip/sample/infra/InventoryRepositoryImpl.java
@@ -51,32 +51,35 @@ public class InventoryRepositoryImpl implements InventoryRepository {
      * @return
      */
     @Override
-    public synchronized Boolean changeInventory(String skuId, Long changeInventory) {
+    public Boolean changeInventory(String skuId, Long changeInventory) {
         // 此处考虑单项目情况下加锁，分布式可以使用Redis实现分布式锁
-        // 获取库存信息
-        String key = InventoryConstants.INVENTORY_REDIS_KEY_PREFIX + skuId;
-        String inventoryJsonStr = redisTemplate.opsForValue().get(key);
-        if (StringUtils.isEmpty(inventoryJsonStr)) {
-            return false;
+        // 根据商品ID加锁
+        synchronized(skuId.intern()) {
+            // 获取库存信息
+            String key = InventoryConstants.INVENTORY_REDIS_KEY_PREFIX + skuId;
+            String inventoryJsonStr = redisTemplate.opsForValue().get(key);
+            if (StringUtils.isEmpty(inventoryJsonStr)) {
+                return false;
+            }
+            JSONObject inventory = JSONObject.parseObject(inventoryJsonStr);
+            // 判断库存是否能够扣除
+            Integer totalInventory = inventory.getInteger(InventoryConstants.TOTAL);
+            Integer sellInventory = inventory.getInteger(InventoryConstants.SELL);
+            if (Objects.isNull(totalInventory)) {
+                return false;
+            }
+            if (Objects.isNull(sellInventory)) {
+                sellInventory = 0;
+            }
+            // 不允许超卖
+            if (totalInventory < sellInventory + changeInventory) {
+                return false;
+            }
+            // 更新库存
+            inventory.put(InventoryConstants.SELL, sellInventory + changeInventory);
+            inventoryJsonStr = JSON.toJSONString(inventory);
+            redisTemplate.opsForValue().set(key, inventoryJsonStr);
+            return true;
         }
-        JSONObject inventory = JSONObject.parseObject(inventoryJsonStr);
-        // 判断库存是否能够扣除
-        Integer totalInventory = inventory.getInteger(InventoryConstants.TOTAL);
-        Integer sellInventory = inventory.getInteger(InventoryConstants.SELL);
-        if (Objects.isNull(totalInventory)) {
-            return false;
-        }
-        if (Objects.isNull(sellInventory)) {
-            sellInventory = 0;
-        }
-        // 不允许超卖
-        if(totalInventory < sellInventory + changeInventory){
-            return false;
-        }
-        // 更新库存
-        inventory.put(InventoryConstants.SELL, sellInventory + changeInventory);
-        inventoryJsonStr = JSON.toJSONString(inventory);
-        redisTemplate.opsForValue().set(key,inventoryJsonStr);
-        return true;
     }
 }
